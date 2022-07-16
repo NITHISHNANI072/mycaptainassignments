546d5f4c2e5da08cb8762aa9486f3e2c429e9366
NITHISHNANI072 <109414794+NITHISHNANI072@users.noreply.github.com>
2022-07-16 15:41:34 +0530

    created extension.py
    this code will check whether the extension of the given file name is python or not

---
+++ extension.py
@@ -0,0 +1,9 @@
+file= input("enter the filename :")
+f= file.split(".")
+y= f[1]
+if y=="py":
+    print("The extension of the file is python")
+else :
+    print("the extension of the fie is not python")
+
+    
